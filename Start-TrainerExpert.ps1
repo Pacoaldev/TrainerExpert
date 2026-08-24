288104d4a6d2c7c6bff613d665a7d692211ad2b4
Paco Lopez Alarte <spanocrypt@gmail.com>
2026-08-24 22:35:59 +0200

    fix: switch to cmd.exe execution to resolve npx process startup issues

--- Start-TrainerExpert.ps1
+++ Start-TrainerExpert.ps1
@@ -1,5 +1,5 @@
 # Start-TrainerExpert.ps1
 $projectDir = "c:\Users\spano\Documents\PROYECTOS\TrainerExpert"
-Start-Process -FilePath "npx" -ArgumentList "-y", "http-server", "-p", "8080" -WorkingDirectory $projectDir -WindowStyle Hidden
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c npx -y http-server -p 8080" -WorkingDirectory $projectDir -WindowStyle Hidden
 Start-Sleep -Seconds 3
 Start-Process -FilePath "C:\Program Files\Perplexity\Comet\Application\comet.exe" -ArgumentList "http://localhost:8080"
